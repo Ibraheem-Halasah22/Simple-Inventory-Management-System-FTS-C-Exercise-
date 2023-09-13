bcc690855dbceb0f9a59ca453bc5f05530cb98dc
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-09-13 08:27:40 +0300

    Basic classes added
    The basic classes product and inventory added with their basic properties

---
+++ Simple Inventory System/Inventory.cs
@@ -0,0 +1,17 @@
+﻿namespace Simple_Inventory_System;
+
+public class Inventory
+{
+    private List<Product> _listOfProducts;
+
+    Inventory()
+    {
+        _listOfProducts = new List<Product>();
+    }
+    public void AddProduct(string name, int quantity, float price)
+    {
+        Product product = new Product(name: name, price:price, quantity: quantity);
+        _listOfProducts.Add(product);
+    }
+        
+}
---
+++ Simple Inventory System/Product.cs
@@ -0,0 +1,38 @@
+﻿namespace Simple_Inventory_System;
+
+public class Product
+{
+    public Product(int quantity, float price, string name)
+    {
+        Quantity = quantity;
+        Price = price;
+        Name = name;
+    }
+
+    private int _quantity;
+    public int Quantity
+    {
+        get => _quantity;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Product Quantity can't be negative");
+            _quantity = value;
+        }
+    }
+
+    public string Name { get; set; }
+
+    private float _price;
+
+    public float Price
+    {
+        get => _price;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Product price can't be negative");
+            _price = value;
+        }
+    }
+}
