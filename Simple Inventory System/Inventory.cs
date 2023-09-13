1402b98b26b4ac1acbfbc37c79d44fca4eb3608f
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-09-13 08:36:37 +0300

    Add product funtionality added
    A "AddProduct" method added to the Inventory class

--- Simple Inventory System/Inventory.cs
+++ Simple Inventory System/Inventory.cs
@@ -8,6 +8,11 @@ public class Inventory
     {
         _listOfProducts = new List<Product>();
     }
-    
+
+    public void AddProduct(int quantity, float price, string name)
+    {
+        var product = new Product(quantity, price, name);
+        _listOfProducts.Add(product);
+    }
         
 }
