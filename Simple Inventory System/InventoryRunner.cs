a0ab1de0d6aea1fee740cc1405c4678624eb6b67
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-09-29 04:36:06 +0300

    inventor runner class added
    a class called InventoryRunner added with a static method to run the system by a console based interface (the interface hasn't programmed yet)
    
    adding the line of code in Program.cs to run the system via the InventoryRunner

---
+++ Simple Inventory System/InventoryRunner.cs
@@ -0,0 +1,16 @@
+﻿namespace Simple_Inventory_System;
+
+public class InventoryRunner
+{
+    public static void RunInventory()
+    {
+        var inventory = new Inventory();
+        
+
+        Console.WriteLine("Welcome to this inventory management system\r");
+        Console.WriteLine("-------------------------------------------\n");
+
+        
+        Console.ReadKey();
+    }
+}
